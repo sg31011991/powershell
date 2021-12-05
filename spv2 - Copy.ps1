17ac1b39f360c303c8c266924f50d939e4b25bf8
ss <you@example.com>
2021-12-05 08:36:04 -0600

    powershell

---
+++ spv2 - Copy.ps1
@@ -0,0 +1,100 @@
+﻿
+Param(
+    [Parameter(Mandatory = $False)]
+    [string]$PersonalToken = "riu4zubftxieoc5rasbwtl5mwcsbouhalzowmpxlemwzvobmj6ja",
+    
+    [Parameter(Mandatory = $False)]
+    [string]$Organisation = "adoanalytics",
+    
+    [Parameter(Mandatory = $False)]
+    [string]$ProjectName = "test adoanalytics",
+ 
+    [Parameter(Mandatory = $False, ParameterSetName = 'Subscription')]
+    [string]$SubscriptionId = "6b099473-a929-4cc7-9f5f-3d238112f0a3",
+
+    [Parameter(Mandatory = $False, ParameterSetName = 'Subscription')]
+    [string]$SubscriptionName = "Azure subscription 1",
+
+    [Parameter(Mandatory = $False)]
+    [string]$TenantId = "bb9d7afc-4ddc-42dd-8518-26777626a8df",
+    
+    [Parameter(Mandatory = $False)]
+    [string]$ApplicationId = "1f98a4f7-5229-4374-b489-3d2d31cde869",
+    
+    [Parameter(Mandatory = $False)]
+    [string]$ApplicationSecret = "JfC7Q~acTE.ozs4XtzJK28m3OmjBsYrO3Sf6f"
+    
+)
+
+$AzureDevOpsAuthenicationHeader = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($personalToken)")) }
+
+## Get ProjectId
+$URL = "https://dev.azure.com/$($organisation)/_apis/projects?api-version=6.0"
+Try {
+    $ProjectNameproperties = (Invoke-RestMethod $URL -Headers $AzureDevOpsAuthenicationHeader -ErrorAction Stop).Value
+    Write-Verbose "Collected Azure DevOps Projects"
+}
+Catch {
+    $ErrorMessage = $_ | ConvertFrom-Json
+    Throw "Could not collect project: $($ErrorMessage.message)"
+}
+$ProjectID = ($ProjectNameproperties | Where-Object { $_.Name -eq $ProjectName }).id
+Write-Verbose "Collected ID: $ProjectID"
+$ConnectionName = "Connection To $ProjectName"
+
+
+    $data = @{
+            SubscriptionId   = $SubscriptionId
+            SubscriptionName = $SubscriptionName
+            environment      = "AzureCloud"
+            scopeLevel       = "Subscription"
+            creationMode     = "Manual"
+        }
+
+# Create body for the API call
+$Body = @{
+    data                             = $data
+    name                             = $ConnectionName
+    type                             = "AzureRM"
+    url                              = "https://management.azure.com/"
+    authorization                    = @{
+        parameters = @{
+            tenantid            = $TenantId
+            serviceprincipalid  = $ApplicationId
+            authenticationType  = "spnKey"
+            serviceprincipalkey = $ApplicationSecret
+        
+        }
+        scheme     = "ServicePrincipal"
+    }
+    isShared                         = $false
+    isReady                          = $true
+    serviceEndpointProjectReferences = @(
+        @{
+            projectReference = @{
+                id   = $ProjectID
+                name = $ProjectName
+            }
+            name             = $ConnectionName
+        }
+    )
+}
+
+$URL = "https://dev.azure.com/$organisation/$ProjectName/_apis/serviceendpoint/endpoints?api-version=6.0-preview.4"
+$Parameters = @{
+    Uri         = $URL
+    Method      = "POST"
+    Body        = ($Body | ConvertTo-Json -Depth 3)
+    Headers     = $AzureDevOpsAuthenicationHeader
+    ContentType = "application/json"
+    Erroraction = "Stop"
+}
+try {
+    Write-Verbose "Creating Connection"
+    $Result = Invoke-RestMethod @Parameters
+    Write-Host "$($Result.name) service connection created"
+}
+Catch {
+    $ErrorMessage = $_ | ConvertFrom-Json
+    Throw "Could not create Connection: $($ErrorMessage.message)"
+}
---
+++ svc-conn-input-1.ps1
@@ -0,0 +1,96 @@
+﻿#GET https://dev.azure.com/{organization}/{project}/_apis/serviceendpoint/endpoints?type={type}&authSchemes={authSchemes}&endpointIds={endpointIds}&owner={owner}&includeFailed={includeFailed}&includeDetails={includeDetails}&api-version=5.1-preview.2
+#create service connection
+
+$Token = "2wmhqumlqhpqu5qo3v342ywqt4c6ez6bx43fpmiwbncytj4dnmuq"
+$Organization = "sgdemo0376"
+$ProjectName = "DEMO-SC-LA"
+
+$Uri = ('https://dev.azure.com/{0}/{1}/_apis/serviceendpoint/endpoints?api-version=5.1-preview.2' -f $Organization, $ProjectName)
+$SecureToken = [system.convert]::ToBase64String([system.text.encoding]::ASCII.GetBytes(":$($Token)"))
+$Params = @{
+    Uri         = $uri
+    Headers     = @{
+        Authorization = "Basic $SecureToken"
+    } 
+    method      = "GET"
+    ContentType = "application/json"
+}
+$value = Invoke-RestMethod @Params
+$objects = $value.value
+$objects | ConvertTo-Json
+########Required information from last part#######################
+$subscriptionId = $Objects.data.subscriptionId | Select-Object -Unique
+$subscriptionName = $Objects.data.subscriptionName | Select-Object -Unique
+$serviceprincipalid = "1f98a4f7-5229-4374-b489-3d2d31cde869"
+$tenantid = "bb9d7afc-4ddc-42dd-8518-26777626a8df"
+$serviceprincipalkey = "JfC7Q~acTE.ozs4XtzJK28m3OmjBsYrO3Sf6f"
+#_______________________Retreive the Azure DevOps Project Id_________________________________
+$Url = ('https://dev.azure.com/{0}/_apis/projects?api-version=6.0' -f $Organization)
+$SecureToken = [system.convert]::ToBase64String([system.text.encoding]::ASCII.GetBytes(":$($Token)"))
+$Params = @{
+    Uri         = $Url
+    Headers     = @{
+        Authorization = "Basic $SecureToken"
+    } 
+    method      = "GET"
+    ContentType = "application/json"
+}
+$value = Invoke-RestMethod @Params
+$object1 = $value.value
+$Object = $object1.id
+
+#__________________________________________________________________________________
+$body = '{
+ 
+  "administratorsGroup": null,
+  "authorization": {
+    "parameters": {
+      "authenticationType": "spnKey",
+      "serviceprincipalid": "' + $serviceprincipalid+ '",
+      "tenantid": "' + $tenantid + '",
+      "serviceprincipalkey": "' + $serviceprincipalkey + '"
+    },
+    "scheme": "ServicePrincipal"
+  },
+  "data": {
+    "creationMode": "Manual",
+    "environment": "AzureCloud",
+    "scopeLevel": "Subscription",
+    "subscriptionId": "' + $subscriptionId + '",
+    "subscriptionName": "' + $subscriptionName + '"
+  },
+  "description": "Description - CloudSkills Sub",
+  "groupScopeId": null,
+  "isReady": true,
+  "isShared": false,
+  "name": "devopsapp",
+  "operationStatus": null,
+  "owner": "Library",
+  "readersGroup": null,
+  "serviceEndpointProjectReferences": [
+    {
+      "description": "Description - CloudSkills Sub",
+      "name": "devopsapp",
+      "projectReference": {
+        "id": "' + $Object + '",
+        "name": "' + $ProjectName + '"
+      }
+    }
+  ],
+  "type": "azurerm",
+  "url": "https://management.azure.com/"
+
+  }'
+  
+  #__________________________________________________________________________________________________________
+ $Uri = ('https://dev.azure.com/{0}/{1}/_apis/serviceendpoint/endpoints?api-version=5.1-preview.2' -f $Organization, $ProjectName)
+ $SecureToken = [system.convert]::ToBase64String([system.text.encoding]::ASCII.GetBytes(":$($Token)"))
+ $Headers     = @{
+        Authorization = "Basic $SecureToken"
+    } 
+     
+  $response = Invoke-RestMethod -Method 'Post' -Uri $Uri -Headers $Headers -Body $body -ContentType "application/json"
+ 
+  write-output $response
+  write-host "Service Connection has been created Successfully" -ForegroundColor Green
+  write-host " New Service Connection name : $($response.name)  " -ForegroundColor Cyan
